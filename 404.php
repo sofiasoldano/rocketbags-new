a1f8d5f9931aa1eeb3e551283b4f42fbe4e61863
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-03-25 13:33:07 -0300

    404

---
+++ 404.php
@@ -0,0 +1,34 @@
+<?php $page_name = "404" ?>
+<?php include('header.php'); ?>
+
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                    <div class="img-container">
+                        <img src="img/404.png" alt="Page not found">
+                    </div>
+                    <div class="info">
+                        <div class="title">Oops, that page <br> doesn't exist!</div>
+                        <p>Sorry for the inconvenience, we couldn't find the page or bag you were looking for. </p>
+                        <p><b>You might find this information useful:</b></p>
+                        <a href="#" class="red-lnk arrow-right">See our Range of Stock Bags</a>
+                        <a href="#" class="red-lnk arrow-right">Get a Custom Tailor-Made</a>
+                        <a href="#" class="btn-full arrow-right">Get a Quick Quote</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bottom">
+                    <p><span class="gray">Not sure what you need?</span></p>
+                    <p>Give us a call on <a href="tel:03337000133">0333 7000 133</a> today!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
