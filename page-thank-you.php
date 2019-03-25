f59d128542cb49cccca0f7de17f472ba815ba60f
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-03-25 13:58:58 -0300

    Thank you

---
+++ page-thank-you.php
@@ -0,0 +1,35 @@
+<?php $page_name = "thank-you" ?>
+<?php include('header.php'); ?>
+
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="top">
+                    <div class="title">Thanks for Getting in touch!</div>
+                    <p>One of our bag gurus will get you within 2 hours<br>(Monday to Friday 9am - 6pm London time)</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                    <div class="info">
+                        <p>If you haven´t yet, you might like to check out our blog of bag related promo ideas. </p>
+                        <a href="#" class="btn-full upper">Check out bag promo ideas</a>
+                        <div class="title">Need Help right now? </div>
+                        <p>The quickest way to get in touch is to call us directly. You can reach us on 0333 7000 133 during UK business hours.</p>
+                        <p><span class="blue">We look forward to talking with you!</span></p>
+                        
+                    </div>
+                    <div class="img-container">
+                        <img src="img/thank-you.png" alt="Thank you">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
