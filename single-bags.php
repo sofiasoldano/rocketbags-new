00841901a846f09973941d8492e04298c40d4884
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-03-25 16:49:02 -0300

    Single Product

---
+++ single-bags.php
@@ -0,0 +1,196 @@
+<?php $page_name = "product" ?>
+<?php include('header.php'); ?>
+
+
+
+<section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-description">
+                    <div class="bg">Bags</div>
+                    <p>Some of the Reasons  <b>Why We Do Bags Better</b></p>
+                </div>
+            </div>    
+            <div class="col-md-12">
+                <ul class="features-list">
+                    <li>
+                        <div class="feature">
+                            <div class="info">
+                                <div class="card-header">
+                                    <div class="title"><b>25 years </b>Experience</div>
+                                    <img class="icon" src="img/icon-experience.png" width="50">
+                                </div>
+                                <div class="description">
+                                    <p>If your custom-made bags aren't up to the same standard as your approved sample, we'll get them remanufactured or returned for a full credit.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="feature">
+                            <div class="info">
+                                <div class="card-header">
+                                    <div class="title"><b>Lowest Price</b> Guarantee</div>
+                                    <img class="icon" src="img/icon-price.png" width="50">
+                                </div>
+                                <div class="description">
+                                    <p>If your custom-made bags aren't up to the same standard as your approved sample, we'll get them remanufactured or returned for a full credit.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="feature">
+                            <div class="info">
+                                <div class="card-header">
+                                    <div class="title"><b>Quality</b> Guarantee</div>
+                                    <img class="icon" src="img/icon-quality.png" width="50">
+                                </div>
+                                <div class="description">
+                                    
+                                    <p>If your custom-made bags aren't up to the same standard as your approved sample, we'll get them remanufactured or returned for a full credit.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="feature">
+                            <div class="info">
+                                <div class="card-header">
+                                    <div class="title"><b>On-time</b> Guarantee</div>
+                                    <img class="icon" src="img/icon-time.png" width="50">
+                                </div>
+                                <div class="description">
+                                    
+                                    <p>If your custom-made bags aren't up to the same standard as your approved sample, we'll get them remanufactured or returned for a full credit.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="feature">
+                            <div class="info">
+                                <div class="card-header">
+                                    <div class="title"><b>Free </b>Design</div>
+                                    <img class="icon" src="img/icon-design.png" width="50">
+                                </div>
+                                <div class="description">
+                                    
+                                    <p>If your custom-made bags aren't up to the same standard as your approved sample, we'll get them remanufactured or returned for a full credit.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>  
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-description">
+                    <p>Clients Are Also <b>Looking At</b></p>
+                </div>
+            </div> 
+        </div>
+        <div class="row products-list">
+            <div class="col-6 col-md-3">
+                <a href="#" class="product">
+                    <div class="product-colours">
+                        <ul>
+                            <li class="loop-colour-sample" style="background-color: #0a0a0a;" title="Anthracite">Anthracite</li>
+                            <li class="loop-colour-sample" style="background-color: #000080;" title="Navy">Navy</li>
+                            <li class="loop-colour-sample" style="background-color: #dd3333;" title="Red">Red</li>
+                        </ul>
+                    </div>
+                    <div class="img-container">
+                        <img src="img/product.png" alt="Backpacks & Rucksacks" width="195">
+                    </div>
+                    <p class="name">Backpacks & Rucksacks </p>
+                    <div class="btn-full upper">View product</div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product">
+                    <div class="img-container">
+                        <img src="img/product.png" alt="Backpacks & Rucksacks" width="195">
+                    </div>
+                    <p class="name">Bags for Life</p>
+                    <div class="btn-full upper">View product</div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product">
+                    <div class="img-container">
+                        <img src="img/product.png" alt="Backpacks & Rucksacks" width="195">
+                    </div>
+                    <p class="name">Beach Bags </p>
+                    <div class="btn-full upper">View product</div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="#" class="product">
+                    <div class="img-container">
+                        <img src="img/product.png" alt="Backpacks & Rucksacks" width="195">
+                    </div>
+                    <p class="name">Backpacks & Rucksacks </p>
+                    <div class="btn-full upper">View product</div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="partners">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title">We Have Created 100% Unique Tailor-Made Bags for <b>These Well-Known Brands: </b> </div>
+                <div class="partners-slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="img/partners/01-British_Gas_logo.png" alt="British Gas">
+                            <img src="img/partners/02-pfizer_logo.png" alt="Pfizer">
+                            <img src="img/partners/03-Wimbledon_logo.png" alt="Wimbledon">
+                            <img src="img/partners/04-client-logo-deutschebank.png" alt="Deutsche Bank">
+                            <img src="img/partners/05-Marks-and-Spencer-Logo.png" alt="Marks and Spencer">
+                            <img src="img/partners/06-logo-herbalife.png" alt="Herbalife">
+                        </li>
+                        <li>
+                            <img src="img/partners/07-Boots_logo.png" alt="Boots">
+                            <img src="img/partners/08-client-logo-thomson.png" alt="Thomson">
+                            <img src="img/partners/09-client-logo-capita.png" alt="Capita">
+                            <img src="img/partners/10-AstonMartin_logo.png" alt="Aston Martin">
+                            <img src="img/partners/11-client-logo-champneys.png" alt="Champneys">
+                            <img src="img/partners/12-client-logo-metro.png" alt="Metro">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section class="banner">
+    <div class="bg">
+        <img src="img/banner1.png">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="offset-lg-4 col-lg-8">
+                <div class="box">
+                    <p class="title">Looking for <b>something unique?</b></p>
+                    <p>If you need a bag that´s <b>100% customised </b>to your brand (and cheaper!) we can <b>tailor-make bags to your exact specifications.</b> Size, shape, colour, utility. <b>It´s all up to you.</b></p>
+                    <a class="red-lnk arrow-right" href="#">Learn More About Tailor Made Bags</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
