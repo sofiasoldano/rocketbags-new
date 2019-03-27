8bddd0ddd7e75f2736b8b5ea0c5e67adb50b7175
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-03-27 17:40:59 -0300

    Blog

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php $page_name = "blog" ?>
+<?php include('header.php'); ?>
+
+<section class="hero">
+    <div class="background"><img src="img/heros/blog.png" alt="Rocket Bags Blog"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="content">
+                    <div class="breadcrumbs">
+                        Breadcrumb > <span class="blue">Blog </span>
+                    </div>
+                    <h1 class="hero-title">Rocket Bags <b>Blog</b></h1>
+                    
+                    <div class="subscription">
+                        <input type="email" class="email" placeholder="Enter your email adress">
+                        <input type="submit" class="btn-full" value="Subscribe Now">
+                    </div>
+                    
+                    <a href="#" class="btn-full arrow-down visible-xs">Explore</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="article main-article">
+                    <div class="img-container">
+                        <img src="img/main-article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum</div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim …</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="article">
+                    <div class="img-container">
+                        <img src="img/article.png" class="img-responsive">
+                    </div>
+                    <div class="info">
+                        <div class="category">Category</div>
+                        <div class="title">Lorem Ipsum lorem ipsum Lorem Ipsum </div>
+                        <div class="author">Author</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur  incididunt ut labore et dolore…</p>
+                        <a href="#" class="btn-full arrow-right ">Read full Post</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pagination">
+                    <ul class="page-numbers">
+                        <li><a class="current" href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a class="next" href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="banner">
+    <div class="bg">
+        <img src="img/banner1.png">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="offset-lg-4 col-lg-8">
+                <div class="box">
+                    <p class="title">Get your brand on the <b>UK's biggest range of 'ready-to-go' </b>promotional bags!</p>
+                    <a class="red-lnk arrow-right" href="#">Find out more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
